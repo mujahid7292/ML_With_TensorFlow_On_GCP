2b1a5335fe94a3110ef10f1041b5231cfa9aec0a
Saifullah Al Mujahid <mujahid7292@gmail.com>
2020-06-24 12:26:02 +0600

    Course: 8 | Week:1 | Lab:2 | Commit:3

---
+++ 08.Image Understanding with TensorFlow on GCP/Week_1/Lab_2_Image_Classification_with_a_Deep_Neural_Network_Model/Practice/mnistmodel_keras_only/trainer/task.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+import os
+import sys
+
+from . import model
+
+def _parse_arguments(argv):
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        '--model_type',
+        help="Which model type to use.",
+        type=str,
+        default='dnn'
+    )
+    
+    parser.add_argument(
+        '--epochs',
+        help='The number of epochs to train.',
+        type=int,
+        default=10
+    )
+    
+    parser.add_argument(
+        '--steps_per_epoch',
+        help='The number of steps per epoch to train.',
+        type=int,
+        default=100
+    )
+    
+    parser.add_argument(
+        '--job-dir',
+        help="Directory where to save the model.",
+        type=str,
+        default='mnistmodel_keras_only/'
+    )
+    
+    return parser.parse_known_args(argv)
+
+def main():
+    """
+    Parse command line arguments and kicks off model training.
+    """
+    args = _parse_arguments(sys.argv[1:])[0]
+    
+    model_layers = model.get_layers(args.model_type)
+    
+    image_model = model.build_model(model_layers, args.job_dir)
+    
+    model_history = model.train_and_evaluate(
+        image_model, args.epochs, args.steps_per_epoch, args.job_dir
+    )
+    
+if __name__ == '__main__':
+    main()
